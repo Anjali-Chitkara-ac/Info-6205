b07e005ea129d910ac78187ad3754ee01f939a34
Anjali-Chitkara-ac <69366199+Anjali-Chitkara-ac@users.noreply.github.com>
2020-11-08 13:46:13 -0800

    removed duplicates

--- Assignments/src/main/java/BacktrackingRecursion/CoinLimited.java
+++ Assignments/src/main/java/BacktrackingRecursion/CoinLimited.java
@@ -9,68 +9,55 @@ Hint- This is basically generate all sub sets (Binary sequence)
  */
 public class CoinLimited {
 
-    public static void getCoinCombinations(int[] coins){
+    public static void getCoinCombinations(int[] coins, int total){
         if(coins.length==0)
             return;
 
+        Set<List<Integer>> set = new HashSet<>();
         int[] result = new int[coins.length];
-        getCoinCombinations(result,coins,0);
+        getCoinCombinations(result,coins,0,set , total);
+        System.out.println("Total combinations possible : " + set.size());
     }
 
-    private static void getCoinCombinations(int[] result, int[] coins, int current){
+    private static void getCoinCombinations(int[] result, int[] coins, int current,Set<List<Integer>> set, int total){
         if(current==result.length){
-            printCombinations(result, coins);
+            printCombinations(result, coins, set, total);
             return;
         }
 
         for(int i=0;i<2;i++){
             result[current] = i;
-            getCoinCombinations(result,coins, current+1);
+            getCoinCombinations(result,coins, current+1,set, total);
         }
     }
 
-    private static void printCombinations(int[] result, int[] coins){
+    private static void printCombinations(int[] result, int[] coins, Set<List<Integer>> set, int total){
         int sum = 0;
         for(int i=0;i<result.length;i++){
-            if(result[i]==1) {
+            if(result[i]==1) { //that means we are selecting that value
                 sum += coins[i];
             }
         }
 
-        if(sum==73){
-            for(int i=0;i<result.length;i++){
-                if(result[i]==1) {
-                    System.out.print(coins[i] + " ");
+        if(sum==total){
+            //add this array to a set
+            List<Integer> temp = new ArrayList<>();
+            for(int i = 0; i < result.length; i ++) {
+                if(result[i] == 1) {
+                    temp.add(coins[i]);
                 }
             }
-            System.out.println();
+
+            if(!set.contains(temp)) {
+                set.add(temp);
+                System.out.println(temp.toString());
+            }
         }
     }
 
-//    private static int[] getCombinations(int[] result, int[]coins){
-//        int[] coinCombination = new int[coins.length];
-//        int sum = 0;
-//        for(int i=0;i<result.length;i++){
-//            if(result[i]==1) {
-//                sum += coins[i];
-//            }
-//        }
-//
-//        if(sum==73){
-//            int idx = 0;
-//            for(int i=0;i<result.length;i++){
-//                if(result[i]==1) {
-//                    //add it to the arr
-//                    coinCombination[idx] = coins[i];
-//                }
-//            }
-//            //set.add(coinCombination);
-//        }
-//        return coinCombination;
-//    }
 
     public static void main(String[] args) {
         int[] coins = {1,1,1,1,1,5,5,5,10,10,10,10, 25,25};
-        getCoinCombinations(coins);
+        getCoinCombinations(coins, 73);
     }
 }
